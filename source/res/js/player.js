5d04b4a98121b7ed98aaac2df282e557d9396c27
Tiger <kirill-1025@mail.ru>
2015-11-30 23:54:05 +0700

    attack animation
    [space]

--- source/res/js/player.js
+++ source/res/js/player.js
@@ -1,11 +1,14 @@
-var legs = Array(9);
+var legs = Array(10);
 var hand = Array(2);
+var kick = Array(4);
 
 var legTime = 0;
 var handTime = 0;
+var attackTime = -1;
 
 var pos = {};
 var ogr = {};
+var deadimg;
 
 function InitPlayer()
 {
@@ -17,8 +20,13 @@ function InitPlayer()
 	pos.move = false;
 	pos.speed = 200;
 	pos.sound = LoadAudio("res/sounds/footsteps.mp3");
+	pos.dead = false;
+
 	for(var i=0; i<9; i++) legs[i] = LoadImage("res/img/player/leg"+(i+1)+".png"); // 201x251
 	for(var i=0; i<2; i++) hand[i] = LoadImage("res/img/player/hand"+(i+1)+".png"); // 201x251
+	for(var i=0; i<4; i++) kick[i] = LoadImage("res/img/player/kick"+(i+1)+".png");
+	legs[9] = LoadImage("res/img/player/leg0.png");
+	deadimg = LoadImage("res/img/player/dead.png"); // 371
 }
 
 function DrawPlayer(f)
@@ -28,8 +36,6 @@ function DrawPlayer(f)
 
 	//SetColor(255,0,0);
 	//DrawRect(x-50,y-125,100,125);
-	//SetColor(0,255,0);
-	//FillRect(x-4,y-4,8,8);
 
 	rexSave();
 	rexTranslate(x,y);
@@ -37,10 +43,28 @@ function DrawPlayer(f)
 	//Scale(0.5);
 	rx.scale(0.5*pos.n,0.5);
 	SetColor(0,0,0,0.4);
-	FillEllipse(-50,-15,100,30);
 
-	DrawImage(hand[Math.trunc(handTime)],-90,-250);
-	DrawImage(legs[Math.trunc(legTime)],-100,-250);
+	if (!pos.dead)
+	{
+		FillEllipse(-50,-15,100,30);
+		if (attackTime>0)
+		{
+			var id = Math.trunc(4-attackTime*4);
+			DrawImage(kick[id],-90,-270);
+			attackTime-=f*10;
+		}
+		else
+		{
+			DrawImage(hand[Math.trunc(handTime)],-90,-250);
+		}
+		if (pos.move){ DrawImage(legs[Math.trunc(legTime)],-100,-250); } else
+		{ DrawImage(legs[9],-100,-250); }
+	}
+	else
+	{
+		FillEllipse(-100,-20,200,40);
+		DrawImage(deadimg,-371/2,-250/2);
+	}
 
 	rexRestore();
 
@@ -48,10 +72,16 @@ function DrawPlayer(f)
 	m = {};
 	m.x = 0;
 	m.y = 0;
-	if (iskey(KEY_A)) if (dist(pos.x-f*pos.speed,pos.y)) { pos.x-=f*pos.speed; pos.n = -1; pos.move = true; }
-	if (iskey(KEY_D)) if (dist(pos.x+f*pos.speed,pos.y)) { pos.x+=f*pos.speed; pos.n = 1; pos.move = true; }
-	if (iskey(KEY_W)) if (dist(pos.x,pos.y-f*pos.speed*0.6)) { pos.y-=f*pos.speed*0.6; pos.move = true; }
-	if (iskey(KEY_S)) if (dist(pos.x,pos.y+f*pos.speed*0.6)) { pos.y+=f*pos.speed*0.6; pos.move = true; }
+
+	if (!pos.dead)
+	{
+		if (iskey(KEY_A) || iskey(KEY_LEFT)) if (dist(pos.x-f*pos.speed,pos.y)) { pos.x-=f*pos.speed; pos.n = -1; pos.move = true; }
+		if (iskey(KEY_D) || iskey(KEY_RIGHT)) if (dist(pos.x+f*pos.speed,pos.y)) { pos.x+=f*pos.speed; pos.n = 1; pos.move = true; }
+		if (iskey(KEY_W) || iskey(KEY_UP)) if (dist(pos.x,pos.y-f*pos.speed*0.6)) { pos.y-=f*pos.speed*0.6; pos.move = true; }
+		if (iskey(KEY_S) || iskey(KEY_DOWN)) if (dist(pos.x,pos.y+f*pos.speed*0.6)) { pos.y+=f*pos.speed*0.6; pos.move = true; }
+		if (iskey(KEY_ENTER)) pos.dead = true;
+		if (iskey(KEY_SPACE)) attackTime = 1;
+	}
 
 	if (pos.move)
 	{
