442dfb82f49d14b400212bf22b8d92beb6ec4f94
Tiger <kirill-1025@mail.ru>
2015-11-30 18:10:01 +0700

    walking

--- source/res/js/player.js
+++ source/res/js/player.js
@@ -1,31 +1,50 @@
-var legs = Array(9); // 0..8
+var legs = Array(9);
 var hand = Array(2);
+
 var legTime = 0;
 var handTime = 0;
 
+var pos = {};
+
 function InitPlayer()
 {
+	pos.x = 0;
+	pos.y = 0;
+	pos.n = 1;
+	pos.move = false;
 	for(var i=0; i<9; i++) legs[i] = LoadImage("res/img/player/leg"+(i+1)+".png"); // 201x251
 	for(var i=0; i<2; i++) hand[i] = LoadImage("res/img/player/hand"+(i+1)+".png"); // 201x251
 }
 
 function DrawPlayer(f)
 {
-	var x = 200;
-	var y = 300;
+	var x = pos.x + GetWidth()/2;
+	var y = pos.y + GetHeight()/2;
 
 	SetColor(255,0,0);
+	DrawRect(x-50,y-125,100,125);
+	SetColor(0,255,0);
+	FillRect(x-4,y-4,8,8);
 
 	rexSave();
+	rexTranslate(x,y);
 
-	Scale(0.5);
-	DrawImage(hand[Math.trunc(handTime)],x-90,y-250);
-	DrawImage(legs[Math.trunc(legTime)],x-100,y-250);
-
-	legTime+=f*12;
-	handTime+=f*4;
-	if (legTime>8) legTime=0;
-	if (handTime>2) handTime=0;
+	//Scale(0.5);
+	rx.scale(0.5*pos.n,0.5);
+	DrawImage(hand[Math.trunc(handTime)],-90,-250);
+	DrawImage(legs[Math.trunc(legTime)],-100,-250);
 
 	rexRestore();
+
+	pos.move = false;
+	if (iskey(KEY_A)) { pos.x-=f*100; pos.n = -1; pos.move = true; }
+	if (iskey(KEY_D)) { pos.x+=f*100; pos.n = 1; pos.move = true; }
+
+	if (pos.move)
+	{
+		legTime+=f*12;
+		handTime+=f*4;
+		if (legTime>8) legTime=0;
+		if (handTime>2) handTime=0;
+	}
 }
---
+++ source/res/js/vectors.js
@@ -0,0 +1,6 @@
+function newVector()
+{
+	var o = {};
+	o.x = 0;
+	o.y = 0;
+}
